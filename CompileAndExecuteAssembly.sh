38f0a9749252527880d5f6a8cc453fbce2bbeb7c
Lipsitium <cptheac@gmail.com>
2026-02-05 01:29:18 +0100

    Update CompileAndExecuteAssembly.sh
    Removed unnecessary variables, polished the code a bit also removed unnecessary code.

--- CompileAndExecuteAssembly.sh
+++ CompileAndExecuteAssembly.sh
@@ -1,22 +1,43 @@
+
+#Copyright (C) 2026  Lipsitium
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
 #Compile and Execute
 ce(){
-        arg1=$1 
-        arg2=$2
-        arg3="exe"
-        last="${@: -1}"
-        directory="./"
-        if [[ "${arg1/d}" == "-m" ]]; then
-                arg3=${last%.s}
+
+        last=${@: -1}
+        last=${last%.s}
+        directory="."
+        if [[ "${1/m}" == "-d" ]]; then
+                directory=${2%/}
         fi
-        if [[ "${arg1/m}" == "-d" ]]; then
-                directory=$2
+        exe="$directory/exe"
+        file="$directory/$last.s"
+
+        if [[ "${1/d}" == "-m" ]]; then
+                exe="$directory/$last"
         fi
-        if [[ ! -e "${directory%/}/${last%.s}.s" ]]; then
-                echo "ERROR: not assembly file"
+        if [[ ! -e $file ]]; then
+                echo "ERROR: not an assembly file"
                 return 1
         fi
-        gcc -nostdlib -o "${directory%/}/$arg3" "${directory%/}/${last%.s}.s"
-        "${directory%/}/${arg3}" 
 
+        gcc -nostdlib -o $exe $file
+        $exe 
 }
 
